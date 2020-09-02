24b34dff72df93c746550a6225a7b6e78c6cab35
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-09-02 16:19:24 -0300

    useEffect

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState([
-    'ReactJS',
-    'React Native',
-  ]);
+  const [techs, setTechs] = useState([]);
 
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
-    setTech([...tech, newTech]);
+    setTechs([...techs, newTech]);
   }
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTechs(JSON.parse(storageTech));
+    }
+  }, [])//pega itens do local storage e joga no JSON pra tela
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);//dá um update se houve alteração ou adição de alguma tech entrar no array techs
 
   return (
     <>
       <ul>
-        {tech.map(t => <li key={t}>{t}</li>)}
+        {techs.map(t => <li key={t}>{t}</li>)}
       </ul>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type='button' onClick={handleAdd}>Adicionar</button>
